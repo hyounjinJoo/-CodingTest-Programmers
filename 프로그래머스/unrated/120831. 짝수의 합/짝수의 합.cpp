1eebbdf8909aa6d1dc26ed0ab775d669e103d1b0
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2023-12-04 14:24:09 +0900

    [level unrated] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120831. 짝수의 합/짝수의 합.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    for(int number = 1; number <= n; ++number)
+    {
+        answer += (number % 2 == 0) ? number : 0;
+    }
+    return answer;
+}
