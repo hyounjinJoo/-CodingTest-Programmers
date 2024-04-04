d157038ba8f469bbcc8134984a8122aca025b49c
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-04-04 09:19:57 +0900

    [level 2] Title: 더 맵게, Time: 218.80 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -8,14 +8,15 @@ int solution(vector<int> scoville, int K) {
     int answer = 0;
     priority_queue<int, vector<int>, greater<int>> PQueue(scoville.begin(), scoville.end());
     
+    int Mixed = 0;
     while(PQueue.size() > 1 && PQueue.top() < K)
     {
-        int left = PQueue.top();
+        Mixed = PQueue.top();
         PQueue.pop();
-        int right = PQueue.top();
+        Mixed = Mixed + (2 * PQueue.top());
         PQueue.pop();
-        
-        PQueue.push(left + (right * 2));
+
+        PQueue.push(Mixed);
         answer++;
     }
     
