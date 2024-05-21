5de39e9b54db4da840efdd317729f120929dc179
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-05-22 08:10:37 +0900

    [level 2] Title: 숫자의 표현, Time: 0.02 ms, Memory: 3.85 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -6,18 +6,15 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     
-    // 약수의 갯수 구하기
-    for(int Num = 2; Num < n; ++Num)
+    for(int Num = 1; Num < n + 1; ++Num)
     {
-        if((false == (n % Num)) && (Num & 1))
+        if(n % Num == 0)
         {
-            ++answer;
+            if(Num % 2 == 1)
+            {
+                ++answer;
+            }
         }
-    }
-    answer += (n & 1) ? 2 : 1;
-    if(1 == n)
-    {
-        answer = 1;
     }
     
     return answer;
