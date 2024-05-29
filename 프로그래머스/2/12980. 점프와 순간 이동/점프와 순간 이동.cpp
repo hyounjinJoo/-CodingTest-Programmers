cc862b12876b1f8b4669d94fd96a37529cdb85aa
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-05-29 20:55:31 +0900

    Update 점프와 순간 이동.cpp
    Previously solved method Add

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -5,3 +5,16 @@ int solution(int Number)
 {        
     return __builtin_popcount(Number);
 }
+
+//int solution(int Number)
+//{
+//    int Answer = 0;
+//    
+//    while(Number)
+//    {
+//        Number &= (Number - 1);
+//        ++Answer;
+//    }
+//    
+//    return Answer;
+//}
