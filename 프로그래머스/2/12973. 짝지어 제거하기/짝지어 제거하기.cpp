abb376cea82f525dba54b4d688da35f1ed34a9e2
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-05-30 13:47:13 +0900

    [level 2] Title: 짝지어 제거하기, Time: 4.93 ms, Memory: 6.39 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,80 +1,13 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include<string>
 using namespace std;
 
 int solution(string s)
 {
-    int answer = 0;
-    stack<char> CharStack;
-    
-    int Size = s.size();
-    for(int Idx = 0; Idx < Size; ++Idx)
-    {
-        if(false == CharStack.empty() && CharStack.top() == s[Idx])
-        {             
-            CharStack.pop();
-        }
-        else
-        {            
-            CharStack.push(s[Idx]);  
-        }
-    }
-    
-    if(CharStack.empty())
-    {
-        answer = 1;
-    }
-    
+    int answer = -1;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
     return answer;
 }
-
-// 첫번째 고민
-/*
-int solution(string s)
-{
-    // 문자가 들어온다
-    // 들어온 문자가 이전 문자와 다른가?
-    // Y : 새로운 문자 시작
-    // 들어온 문자가 이전 문자와 동일한가?
-    // Y : 삭제할 준비가 되었다.
-    // 들어온 문자가 이전 문자와 다른가?
-    // Y : 인덱스를 -2만큼 이전으로 바꾼다.
-    // 삭제된 문자열이 있다면 다시 반복을 수행한다.
-    string Test;
-    bool bNeedRepeat = false;
-    int Limit = s.size() - 1;
-    char PrevChar = NULL;
-    bool bFirstNotEqual = false;
-    bool bSecondEqual = false;
-    bool bThirdNotEqual = false;
-    for(int Idx = 0; true == bNeedRepeat; ++Idx)
-    {
-        if(NULL == PrevChar && 0 == Idx)
-        {
-            PrevChar = s[Idx];
-            
-        }
-        
-        if((Idx == Limit) && true == bNeedRepeat)
-        {
-            Idx = 0;
-            s = Test;
-            
-            if(true == s.empty())
-            {
-                answer = 1;
-                break;
-            }
-            
-            Test.clear();
-            Limit = s.size() - 1;
-            PrevChar = NULL;
-            
-            bFirstNotEqual = false;
-            bSecondEqual = false;
-            bThirdNotEqual = false;
-        }
-    }
-}
-*/
