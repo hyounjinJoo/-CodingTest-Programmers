10d4e330c87cc1d5d126662b2eb3548a3a1c4962
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-05 00:10:18 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 361.92 ms, Memory: 240 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -0,0 +1,70 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+/* 케이스 20~23 통과X
+vector<int> solution(vector<int> _InNumbers) {
+    vector<int> OutAnswer;
+    
+    int SizeOfNums = _InNumbers.size();
+    int ClosestGreaterNum = -1;
+    for(int Idx = 0; Idx < SizeOfNums; ++Idx)
+    {
+        // 나보다 큰 수 중 가장 가까이인 수.
+        for(int IdxAfterMe = Idx + 1; IdxAfterMe < SizeOfNums; ++IdxAfterMe)
+        {
+            if(_InNumbers[Idx] < _InNumbers[IdxAfterMe])
+            {
+                ClosestGreaterNum = _InNumbers[IdxAfterMe];
+                break;
+            }
+        }        
+            
+        OutAnswer.push_back(ClosestGreaterNum);
+        ClosestGreaterNum = -1;
+    }
+    
+    return OutAnswer;
+}
+*/
+#define NotFound -1
+
+vector<int> solution(vector<int> _InNumbers) {
+    vector<int> OutAnswer(_InNumbers.size(), NotFound);
+    int SizeOfNums = _InNumbers.size();
+    
+    for(int Idx = SizeOfNums - 2; Idx >= 0; --Idx)
+    {
+        // 나보다 큰 수 중 가장 가까이인 수.
+        for(int IdxAfterMe = Idx + 1; IdxAfterMe < SizeOfNums; ++IdxAfterMe)
+        {
+            // 나보다 큰 수가 발견되었다
+            if(_InNumbers[Idx] < _InNumbers[IdxAfterMe])
+            {
+                OutAnswer[Idx] = _InNumbers[IdxAfterMe];
+                break;
+            }
+            // 발견하지 못했다면... 내가 크거나 같은 경우이다
+            else
+            {
+                // 나 다음 정답이 -1이면
+                if(NotFound == OutAnswer[IdxAfterMe])
+                {
+                    // 일단 찾지 못한것으로 체크한다.
+                    OutAnswer[Idx] = NotFound;
+                    break;
+                }     
+                // 나 다음에서 정답으로 기록된 것이 나보다 크다면 변경해준다.
+                else if(_InNumbers[Idx] < OutAnswer[IdxAfterMe])
+                {
+                    OutAnswer[Idx] = OutAnswer[IdxAfterMe];
+                    break;
+                }
+            }
+        }
+    }
+    
+    return OutAnswer;
+}
