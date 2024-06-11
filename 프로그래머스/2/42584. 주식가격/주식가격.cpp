99f26c735bd333fe64468af9185b95cec018e947
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-11 22:03:01 +0900

    [level 2] Title: 주식가격, Time: 14.51 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <limits>
+
+using namespace std;
+
+vector<int> solution(vector<int> Prices) {
+    vector<int> Answer;
+    int Size = Prices.size();
+    
+    for(int OutIdx = 0; OutIdx < Size; ++OutIdx)
+    {
+        int Time = 0;
+        for(int InIdx = OutIdx + 1; InIdx < Size; ++InIdx)
+        {
+            ++Time;
+            if(Prices[OutIdx] > Prices[InIdx])
+            {
+                break;
+            }            
+        }
+        
+        Answer.push_back(Time);
+    }
+    
+    return Answer;
+}
