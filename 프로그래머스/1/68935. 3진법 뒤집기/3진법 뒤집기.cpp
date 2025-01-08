ec52d9cc26e8d283c4ac865fec551c085ca58a1e
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2025-01-08 10:31:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -1,26 +1,24 @@
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    vector<int> tri;
+    vector<int> triVector;
     while(n > 0)
     {
-        int number = n % 3;
-        tri.push_back(n % 3);
+        triVector.push_back(n % 3);
         n /= 3;
-        if(n == 1)
-        {
-            tri.push_back(1);
-            break;
-        }
-    }
-    for(int i = 0; i < tri.size(); i++)
-    {
-        answer += pow(3, tri.size() - i - 1) * tri[i];
     }
     
+    int powNumber = 1;
+    
+    while(!triVector.empty()) 
+    {
+        answer += powNumber * triVector.back();
+        triVector.pop_back();
+        powNumber *=3;
+    }
+
     return answer;
 }
