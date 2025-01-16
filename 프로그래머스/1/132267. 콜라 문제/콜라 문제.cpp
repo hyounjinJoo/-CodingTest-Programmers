32fa09c28563672b52f795866b627a008e718f1e
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2025-01-16 09:23:34 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    // 콜라 새 거 개수 n
+    // 병 a개를 가져다 주면 새 거 b병을 준다.    
+    int NewColar = 0;
+    while(n >= a)
+    {
+        // 받을 수 있는 병의 개수 answer
+        // 새로 받은 거는 다시 새 거에 추가된다.
+        // 1. 받을 수 있는 개수를 미리 구한다.
+        // 2. 새로 받은 것을 기록한다.(몫)
+        // 3. answer에 더해준다.
+        // 4. 전체 개수를 갱신한다. (2.의 나머지 + 몫)
+        // 5. 다시 이를 n이 이상일 동안 반복한다.
+        NewColar = n / a * b;
+        answer += NewColar;
+        n = (n % a) + NewColar;        
+    }
+        
+    return answer;
+}
