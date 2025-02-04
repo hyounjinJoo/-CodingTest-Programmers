10f6e76c4475255bb4f03c7d8cf4550906d58d7b
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2025-02-04 09:49:28 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    // n : 학생 수
+    // lost : 도난당한 자들의 번호
+    // reserve : 여유 1개씩 더 가져온 친구
+    // 번호의 앞, 뒤에만 빌려줄 수 있음.
+    // 도난당한 자의 번호가 reserve에 있을 수 있음.
+    // 번호는 1부터 시작
+    
+    // 학생 수 만큼 배열 선언
+    vector<int> Student(n, 1);
+    for(int IDX = 0; IDX < lost.size(); ++IDX)
+    {
+        --Student[lost[IDX] - 1];
+    }
+    
+    for(int IDX = 0; IDX < reserve.size(); ++IDX)
+    {
+        ++Student[reserve[IDX] - 1];
+    }
+    
+    for(int IDX = 0; IDX < Student.size(); ++IDX)
+    {
+        if(Student[IDX] >= 1)
+        {
+            continue;            
+        }
+        
+        // 학생을 자기 앞 뒤랑 비교
+        if(IDX - 1 >= 0 && Student[IDX - 1] > 1)
+        {
+            --Student[IDX - 1];
+            ++Student[IDX];
+        }
+        else if(IDX + 1 < Student.size() && Student[IDX + 1] > 1)
+        {
+            --Student[IDX + 1];
+            ++Student[IDX];            
+        }        
+    }
+    
+    int answer = Student.size();
+    for(int IDX = 0; IDX < Student.size(); ++IDX)
+    {
+        if(0 == Student[IDX])
+        {
+            --answer;
+        }
+    }
+    
+    return answer;
+}
