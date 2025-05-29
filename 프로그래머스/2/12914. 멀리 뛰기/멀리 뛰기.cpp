0f5f09befb7459f2bbae63f9ea51abc59f274ac2
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2025-05-29 11:49:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -1,44 +1,38 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
-
-long long Fibonazzi(long long n);
-long long Fibonazzi_Tail(long long n);
-
-long long Fibonazzi(long long n)
+long long solution(int n)
 {
-    return Fibonazzi_Tail(n);
-}
-
-long long Fibonazzi_Tail(long long n)
-{
-    if(0 == n)
+    // 한 번에 1칸 혹은 2칸을 뛸 수 있다.
+    // 칸이 총 1칸이면
+    // 경우의 수는 1로 1개
+    // 칸이 2칸이면
+    // 경우의 수는 1-1, 2로 2개
+    // 칸이 3칸이면
+    // 경우의 수는 1-1-1, 1-2, 2-1 => 3개
+    // 칸이 4칸이면
+    // 경우의 수는 1-1-1-1, ... 예시와 동일 => 5개
+    // 칸이 5칸이면
+    // 경우의 수는 8개
+    // 1-1-1-1-1
+    // 1-1-1-2
+    // 1-1-2-1
+    // 1-2-1-1
+    // 1-2-2
+    // 2-1-1-1
+    // 2-1-2
+    // 2-2-1
+    // 즉, f(n) = f(n - 2) + f(n - 1)가 되는 경향을 뛴다.
+        
+    vector<long long> Fibonazzi(n + 1);
+    
+    Fibonazzi[0] = 1;
+    Fibonazzi[1] = 1;
+    for(int IDX = 2; IDX <= n; ++IDX)
     {
-        return 0;
-    }
-    else if(1 == n)
-    {
-        return 1;
-    }
-    else
-    {
-        return Fibonazzi_Tail(n - 1) + Fibonazzi_Tail(n - 2);
-    }
-}
-
-
-long long solution(int n) {
-    vector<long long> FibonazziARR;
-    FibonazziARR.push_back(1);
-    FibonazziARR.push_back(1);
-    long long Temp = 0;
-    for(int Idx = 2; Idx <= n; ++Idx)
-    {
-        Temp = (FibonazziARR[Idx - 1] + FibonazziARR[Idx - 2]) % 1234567;
-        FibonazziARR.push_back(Temp);
+        Fibonazzi[IDX] = (Fibonazzi[IDX - 2] + Fibonazzi[IDX -1]) % 1234567;
     }
     
-    return FibonazziARR[n];
+    return Fibonazzi[n];
 }
