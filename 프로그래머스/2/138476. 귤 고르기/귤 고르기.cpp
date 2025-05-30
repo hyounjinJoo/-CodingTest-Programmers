dd435862bfc20943d42d053d52ea937bc83ff30e
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2025-05-30 10:56:24 +0900

    [level 2] Title: 귤 고르기, Time: 40.56 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,38 +1,42 @@
-#include <string>
 #include <vector>
 #include <map>
 #include <algorithm>
 
 using namespace std;
 
-int solution(int k, vector<int> tangerine) {
-    int Answer = 0;
-    
-    map<int, int> TanMap;
-    for(int Tan : tangerine)
+int solution(int k, vector<int> tangerine) 
+{
+    // 귤을 크기별로 분류하여 개수를 센다
+    map<int, int> MapTangerine;
+    for(int& Tan : tangerine)
     {
-        ++TanMap[Tan];
+        ++MapTangerine[Tan];
     }
     
-    vector<int> ForSort;
-    for(auto Pair : TanMap)
+    // 상자에 개수가 많은 종류부터 담으면 
+    // 상자에 담긴 종류가 최소화 되므로
+    // 종류별 개수를 배열에 담아 내림차순으로 정렬해준다.
+    vector<int> BigToSmallArray;
+    for(auto Pair : MapTangerine)
     {
-        ForSort.push_back(Pair.second);
+        BigToSmallArray.push_back(Pair.second);
     }
     
-    sort(ForSort.begin(), ForSort.end(), greater<int>());
+    sort(BigToSmallArray.begin(), BigToSmallArray.end(), greater<int>());
     
-    int Count = 0;
-    for(int Idx = 0; Idx < ForSort.size(); ++Idx)
+    int CountInBox = 0, Answer = 0;
+    for(int IDX = 0; IDX < BigToSmallArray.size(); ++IDX)
     {
-        if(Count >= k)
+        // 상자에 담긴 귤 수가 k개 이상이면 꽉찼으니 정지
+        if(CountInBox >= k)
         {
             break;
         }
         
+        // 종류 수 더해주고
+        // 현재 상자에 담긴 귤 수를 갱신한다.
         ++Answer;
-        Count += ForSort[Idx];
-        
+        CountInBox += BigToSmallArray[IDX];
     }
     
     return Answer;
